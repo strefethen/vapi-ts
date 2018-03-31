128b92ca7a52d09d44d194575ab311d7d1902f13
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-31 08:23:09 -0700

    Changes to make the example more generic and follow a similar pattern used in vsphere-node.

--- index.ts
+++ index.ts
@@ -4,28 +4,38 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 let host = "https://sc2-rdops-vm06-dhcp-195-173";
 
+var username = 'administrator@vsphere.local',
+    password = 'Admin!23',
+    url = `${host}/rest/com/vmware/cis/session`;
+
 // Setup Basic Auth request to the vSphere REST endpoint
-let req = new msRest.WebResource(`${host}/rest/com/vmware/cis/session`, 
+let authRequest = new msRest.WebResource(`${host}/rest/com/vmware/cis/session`, 
 	"POST",
 	null,
 	undefined,
 	{  
-		"Accept": "application/json",
-		"Content-Type": "application/json",
-		"Authorization": "Basic " + new Buffer("administrator@vsphere.local:Admin!23").toString("base64")
+		"Authorization": "Basic " + new Buffer(`${username}:${password}`).toString("base64")
 	}
 );
 
 async function auth() {
-  let value = await msRest.dispatchRequest(req);
+  let value = await msRest.dispatchRequest(authRequest);
 }
 
-async function listVms() {
-  let list: msRest.HttpOperationResponse = await msRest.dispatchRequest(new msRest.WebResource(`${host}/rest/vcenter/vm`));
-	console.log(list.bodyAsJson);
+async function get(url: string) {
+  return await msRest.dispatchRequest(new msRest.WebResource(url));
 }
 
 auth().then(() => {
-	listVms();
+	// Fetch a list of VMs
+	get(`${host}/rest/vcenter/vm`).then((vms) => {
+		console.log(vms.bodyAsText);
+		// Fetch details for a single VM, if we recieved a list
+		get(`${host}/rest/vcenter/vm/${vms.bodyAsJson.value[0].vm}`).then((vm) => {
+			console.log(`\nDetails of VM: ${vms.bodyAsJson.value[0].vm}`);
+			console.log(vm.bodyAsText);
+		});
+	}).catch((error) => {
+		console.log(error);
+	});
 });
-
