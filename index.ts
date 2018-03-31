38949d582e024fbb0186fec8435556439bc7a727
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-30 21:04:00 -0700

    Fix tabs

--- index.ts
+++ index.ts
@@ -4,27 +4,28 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
 let host = "https://sc2-rdops-vm06-dhcp-195-173";
 
+// Setup Basic Auth request to the vSphere REST endpoint
 let req = new msRest.WebResource(`${host}/rest/com/vmware/cis/session`, 
-    "POST",
-    null,
-    undefined,
-    {  
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Basic " + new Buffer("administrator@vsphere.local:Admin!23").toString("base64")
-    }
+	"POST",
+	null,
+	undefined,
+	{  
+		"Accept": "application/json",
+		"Content-Type": "application/json",
+		"Authorization": "Basic " + new Buffer("administrator@vsphere.local:Admin!23").toString("base64")
+	}
 );
 
 async function auth() {
-    let value = await msRest.dispatchRequest(req);
+  let value = await msRest.dispatchRequest(req);
 }
 
 async function listVms() {
-    let list = await msRest.dispatchRequest(new msRest.WebResource(`${host}/rest/vcenter/vm`));
-    console.log(list.bodyAsJson);
+  let list: msRest.HttpOperationResponse = await msRest.dispatchRequest(new msRest.WebResource(`${host}/rest/vcenter/vm`));
+	console.log(list.bodyAsJson);
 }
 
 auth().then(() => {
-    listVms();
+	listVms();
 });
 
